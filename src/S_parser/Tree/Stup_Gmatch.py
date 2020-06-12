88a56200efbed77102a30701dc5a7ed383a5dc0e
anorak6435 <stefan.beckers14@gmail.com>
2020-06-12 09:50:01 +0200

    working on Selector and Hook test

--- src/S_parser/Tree/Stup_Gmatch.py
+++ src/S_parser/Tree/Stup_Gmatch.py
@@ -6,3 +6,6 @@ class Goption:
     def __init__(self, _selector, val):
         self.selector = _selector
         self.value = val
+
+    def __repr__(self):
+        return "select:'{}' with value:'{}'".format(self.selector, self.value)
--- src/S_parser/Tree/Stup_Token_Selector.py
+++ src/S_parser/Tree/Stup_Token_Selector.py
@@ -2,7 +2,7 @@ from S_parser.Tree.Stup_Gmatch import Goption
 # this class will select the tokens to make an ast
 class TokenSelector:
     def __init__(self, toks, entryPoint):
-        self.options = []
+        self.rules = [] # a list of rules we found
         self.tokenIndex = 0 # the tokenIndex
         self.tokens = toks
         self.entryPoint = entryPoint
@@ -17,20 +17,27 @@ class TokenSelector:
             Found = False
             # track the index of the rule_declar
             ruleIndex = 0
-            
+            temp_tokens = []
             # check for rule declarations
             match = True
             while match:
+                match = False
                 if self.rule_declar[ruleIndex].selector == "name":
                     match = self.tokens[self.tokenIndex].match_name(self.rule_declar[ruleIndex].value)
+                    # add tokens to the temporary storage to use in the declaration
+                    temp_tokens.append(self.tokens[self.tokenIndex])
                 elif self.rule_declar[ruleIndex].selector == "value":
                     match = self.tokens[self.tokenIndex].match_value(self.rule_declar[ruleIndex].value)
                 # check if this part of the Grammar did match:
                 if not match:
                     # did not match
-                    raise SyntaxError("There was a mismatch in the grammar at:'{}'".format())
-                
-                match = False
+                    raise SyntaxError("There was a mismatch in the grammar at:'{}'".format(self.rule_declar[ruleIndex]))
+                # advance the rule an the tokenIndex
+                ruleIndex += 1
+                self.tokenIndex += 1
+                if ruleIndex == len(self.rule_declar):
+                    # make the declaration and add to self.rules
+                    print("found a rule!")
 
     # check tokens against a checklist    
     def checklist(self, checklist):
